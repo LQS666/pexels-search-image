7e2dd4f412646413d0a09723f4a450f76b48efbd
Paweł Turkot <p.turkot123@gmail.com>
2019-03-19 20:08:42 +0100

    Add typing input effect

--- src/assets/script.js
+++ src/assets/script.js
@@ -1,1 +1,30 @@
-document.getElementById("search").placeholder = `Wyszukaj zdjęcia`;
+const inputPlaceholder = document.getElementById("search");
+const txt = [
+    "Wyszukaj zdjęcia",
+    "Wpisz frazę",
+    "np. forest, animals, sea"
+]
+let activeLetter = -10;
+let activeText = 0;
+
+const TypingInput = () => {
+    if (activeLetter >= 0) {
+        inputPlaceholder.placeholder += txt[activeText][activeLetter];
+    }
+    activeLetter++;
+    if (activeLetter === txt[activeText].length) {
+
+        activeText++;
+        if (activeText === txt.length) return;
+
+        return setTimeout(() => {
+            activeLetter = -10;
+            inputPlaceholder.placeholder = '';
+            TypingInput();
+        }, 2000)
+    }
+    setTimeout(TypingInput, 100)
+
+}
+
+TypingInput();
